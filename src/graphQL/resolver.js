27c9e93ad58f70c950f1b5e8d9c928d76fa7b178
Vu Tung Duong <tungduonghgg123@gmail.com>
2021-05-23 20:23:35 +0700

    Deduct expense when making a transaction

--- src/graphQL/resolver.js
+++ src/graphQL/resolver.js
@@ -7,6 +7,38 @@ const getUserId = request => {
     throw new Error("userid is needed in the header");
   return request.headers.userid;
 };
+const updateExpensePlan = async (id, transaction) => {
+  // get expense list for this month: (workaround: just get the first quiz)
+  const quiz = (await findOne({ _id: ObjectId(id) })).quizs[0];
+  checkForCategoryInExpense(id, transaction, quiz.monthlyExpense);
+  checkForCategoryInExpense(id, transaction, quiz.limitExpense, "limitExpense");
+};
+const checkForCategoryInExpense = (
+  id,
+  transaction,
+  expense,
+  kind = "monthlyExpense"
+) => {
+  const amount = transaction.amount;
+  const category = transaction.category.name;
+  if (Array.isArray(expense)) {
+    expense.forEach((_expense, index) => {
+      if (_expense.category.name === category) {
+        // update the database right away
+        update(
+          {
+            _id: ObjectId(id)
+          },
+          {
+            $inc: {
+              ["quizs.0." + kind + "." + index + ".currentAmount"]: amount
+            }
+          }
+        );
+      }
+    });
+  }
+};
 module.exports = {
   user: async (_, request) => {
     const id = getUserId(request);
@@ -24,6 +56,10 @@ module.exports = {
         $push: { transactions: transaction }
       }
     );
+    // TODO: make this to O(1) instead of O(n)
+    // based on category, loop through expenseList's category and increase if matches
+    // TODO: this can be run independently after finished a transaction.
+    updateExpensePlan(id, transaction);
     return transaction;
   },
   registerUser: async ({ input }) => {
